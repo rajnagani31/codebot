fecc77ffca0c7160b693e5cd4b5b8ee2c35436cc
rajnagani31 <rajnagani331@gmail.com>
2026-06-12 22:10:21 +0530

    test coderabbit

--- apps/backend/agents/test/demo.py
+++ apps/backend/agents/test/demo.py
@@ -1,1 +1,13 @@
 # demo testcase
+from locale import currency
+
+supplier = None
+currencyr_base = 'INR'
+currency_company = 'USD'
+supplier_currency = "EUR"
+
+if not supplier:
+    supplier_currency = currency_company
+
+if supplier:
+    supplier_currency = supplier_currency
